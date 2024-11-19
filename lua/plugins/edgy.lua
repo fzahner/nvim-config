a0c78f46d6ce1e65c3945d236390a6f080637436
Fabio Zahner <fabio.zahner@ost.ch>
2024-11-19 11:26:07 +0100

    added edgy.nvim for window order

---
+++ lua/plugins/edgy.lua
@@ -0,0 +1,63 @@
+return {
+	"folke/edgy.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.opt.laststatus = 3
+		vim.opt.splitkeep = "screen"
+	end,
+	opts = {
+		animate = {
+			enabled = false,
+			fps = 100, -- frames per second
+			cps = 120, -- cells per second
+		},
+		wo = {
+			-- Setting to `true`, will add an edgy winbar.
+			-- Setting to `false`, won't set any winbar.
+			-- Setting to a string, will set the winbar to that string.
+			winbar = false,
+		},
+		bottom = {
+			-- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+			{
+				ft = "toggleterm",
+				size = { height = 0.35 },
+				-- exclude floating windows
+				filter = function(buf, win)
+					return vim.api.nvim_win_get_config(win).relative == ""
+				end,
+			},
+			{ -- lsps, problems
+				ft = "trouble",
+				size = { height = 0.35 },
+				filter = function(_buf, win)
+					return vim.w[win].trouble.mode == "diagnostics" or vim.w[win].trouble.mode == "lsp"
+				end,
+			},
+		},
+		left = {
+			{
+				ft = "NvimTree",
+				size = { width = 0.15 },
+			},
+			{ -- symbols
+				ft = "trouble",
+				size = { height = 0.35 },
+				filter = function(_buf, win)
+					return vim.w[win].trouble.mode == "symbols"
+				end,
+			},
+		},
+		right = {
+
+			{
+				ft = "help",
+				size = { width = 0.4 },
+				-- only show help buffers
+				filter = function(buf)
+					return vim.bo[buf].buftype == "help"
+				end,
+			},
+		},
+	},
+}
