3ffc857337f3ff271955591e3bf5b3b3878dc391
Fabio Zahner <zahnerfabio@gmail.com>
2025-06-18 16:30:29 +0200

    added some custom grous

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -16,16 +16,16 @@ return {
 			{ "m:", desc = "Preview mark" },
 			{ "m}", desc = "Next bookmark (same type)" },
 			{ "m{", desc = "Previous bookmark (same type)" },
-			{ "m0", desc = "Add bookmark group 0" },
-			{ "m1", desc = "Add bookmark group 1" },
-			{ "m2", desc = "Add bookmark group 2" },
-			{ "m3", desc = "Add bookmark group 3" },
-			{ "m4", desc = "Add bookmark group 4" },
-			{ "m5", desc = "Add bookmark group 5" },
-			{ "m6", desc = "Add bookmark group 6" },
-			{ "m7", desc = "Add bookmark group 7" },
-			{ "m8", desc = "Add bookmark group 8" },
-			{ "m9", desc = "Add bookmark group 9" },
+			{ "m0", desc = "Add group 0 (annotate)" },
+			{ "m1", desc = "Add group 1 (TODO)" },
+			{ "m2", desc = "Add group 2 (Done)" },
+			{ "m3", desc = "Add group 3" },
+			{ "m4", desc = "Add group 4" },
+			{ "m5", desc = "Add group 5" },
+			{ "m6", desc = "Add group 6" },
+			{ "m7", desc = "Add group 7" },
+			{ "m8", desc = "Add group 8" },
+			{ "m9", desc = "Add group 9" },
 
 			-- Delete marks group
 			{ "dm", group = "Delete Marks" },
@@ -33,16 +33,16 @@ return {
 			{ "dm-", desc = "Delete all marks on current line" },
 			{ "dm<space>", desc = "Delete all marks in buffer" },
 			{ "dm=", desc = "Delete bookmark under cursor" },
-			{ "dm0", desc = "Delete bookmark group 0" },
-			{ "dm1", desc = "Delete bookmark group 1" },
-			{ "dm2", desc = "Delete bookmark group 2" },
-			{ "dm3", desc = "Delete bookmark group 3" },
-			{ "dm4", desc = "Delete bookmark group 4" },
-			{ "dm5", desc = "Delete bookmark group 5" },
-			{ "dm6", desc = "Delete bookmark group 6" },
-			{ "dm7", desc = "Delete bookmark group 7" },
-			{ "dm8", desc = "Delete bookmark group 8" },
-			{ "dm9", desc = "Delete bookmark group 9" },
+			{ "dm0", desc = "Delete group 0" },
+			{ "dm1", desc = "Delete group 1" },
+			{ "dm2", desc = "Delete group 2" },
+			{ "dm3", desc = "Delete group 3" },
+			{ "dm4", desc = "Delete group 4" },
+			{ "dm5", desc = "Delete group 5" },
+			{ "dm6", desc = "Delete group 6" },
+			{ "dm7", desc = "Delete group 7" },
+			{ "dm8", desc = "Delete group 8" },
+			{ "dm9", desc = "Delete group 9" },
 		})
 		require("marks").setup({
 			-- whether to map keybinds or not. default true
@@ -51,16 +51,24 @@ return {
 			builtin_marks = { ".", "<", ">", "^" },
 			refresh_interval = 250,
 			sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+			excluded_filetypes = { "NvimTree" },
 			-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
 			-- sign/virttext. Bookmarks can be used to group together positions and quickly move
 			-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
 			-- default virt_text is "".
 			bookmark_0 = {
-				sign = "⚑",
-				virt_text = "hello world",
+				sign = "󰙆",
 				-- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
 				-- defaults to false.
-				annotate = false,
+				annotate = true,
+			},
+			bookmark_1 = {
+				sign = "󰄗",
+				virt_text = "TODO",
+			},
+			bookmark_2 = {
+				sign = "",
+				virt_text = "Done",
 			},
 		})
 	end,
