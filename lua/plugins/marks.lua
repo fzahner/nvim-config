d737ec9d6f9be5d9e5e522d3df3f30d618c9e2f7
Fabio Zahner <zahnerfabio@gmail.com>
2025-06-30 17:14:53 +0200

    fixes for marks.lua

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -51,7 +51,7 @@ return {
 			builtin_marks = { ".", "<", ">", "^" },
 			refresh_interval = 250,
 			sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-			excluded_filetypes = { "NvimTree" },
+			excluded_filetypes = { "NvimTree", "DressingInput" },
 			-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
 			-- sign/virttext. Bookmarks can be used to group together positions and quickly move
 			-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,15 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    preset = "classic", -- modern, helix, classic
-    win = {
-        padding = { 1, 2 },
-    },
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "classic", -- modern, helix, classic
+		win = {
+			padding = { 1, 2 },
+		},
+		triggers = {
+			{ "<auto>", mode = "nixo" },
+			-- Custom triggers for marks
+			{ "m", mode = "n" },
+		},
+	},
 }
