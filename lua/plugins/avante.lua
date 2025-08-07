0f4dd25f11cd6210578e7f43236c12b20242beec
Fabio Zahner <zahnerfabio@gmail.com>
2025-08-07 18:22:35 +0200

    nnvm avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,7 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
+	enabled = false,
 	version = false, -- Never set this value to "*"! Never!
 	build = "make",
 	opts = {
@@ -46,35 +47,4 @@ return {
 			ft = { "markdown", "Avante" },
 		},
 	},
-	config = function()
-		-- Function which allows switching of ollama model
-		local function switch_ollama_model()
-			local models = {
-				{ name = "Local fast Qwen2", endpoint = "http://localhost:11434", model = "qwen2.5-coder:0.5b" },
-				{ name = "Local GPT-OSS 20B", endpoint = "http://localhost:11434", model = "gpt-oss:20b" },
-				-- TODO	{ name = "Remote Powerful", endpoint = "http://192.168.1.100:11434", model = "gpt-oss:120b" },
-			}
-
-			vim.ui.select(models, {
-				prompt = "Select Ollama Model:",
-				format_item = function(item)
-					return item.name
-				end,
-			}, function(choice)
-				if choice then
-					require("avante.config").override({
-						providers = {
-							ollama = {
-								endpoint = choice.endpoint,
-								model = choice.model,
-							},
-						},
-					})
-					print("Switched to: " .. choice.name)
-				end
-			end)
-		end
-
-		-- vim.keymap.set("n", "<leader>am", switch_ollama_model, { desc = "Switch Avante model" })
-	end,
 }
